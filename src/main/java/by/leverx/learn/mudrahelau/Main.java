961dc579aa343c9cda6ee1ff34c3840b59dee55c
Viktar Mudrahelau <prespo@tut.by>
2020-05-15 19:10:26 +0300

    refactoring

--- src/main/java/by/leverx/learn/mudrahelau/Main.java
+++ src/main/java/by/leverx/learn/mudrahelau/Main.java
@@ -5,36 +5,38 @@ import by.leverx.learn.mudrahelau.model.building.DogsAviary;
 import by.leverx.learn.mudrahelau.model.building.TrainingGround;
 import by.leverx.learn.mudrahelau.model.dog.RandomDogCreator;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * @author Viktar on 10.05.2020
  */
 public class Main {
+
+    private static final int NUMBER_OF_AVIARIES = 5;
 
     public static void main(String[] args) {
 
         Farm farm = Farm.getInstance();
 
         TrainingGround trainingGround = new TrainingGround();
-
         farm.setTrainingGround(trainingGround);
 
-        DogsAviary aviary = new DogsAviary();
-        DogsAviary aviary2 = new DogsAviary();
-        DogsAviary aviary3 = new DogsAviary();
-        DogsAviary aviary4 = new DogsAviary();
-
-
-        farm.addAviary(aviary);
-        farm.addAviary(aviary2);
-        farm.addAviary(aviary3);
-        farm.addAviary(aviary4);
-
-
-        for (int i = 0; i < farm.getDogsAviaries().size(); i++) {
-            farm.getDogsAviaries().get(i).addDog(RandomDogCreator.createRandomGod());
-        }
+        farm.setDogsAviaries(generateAviariesWithDogs(NUMBER_OF_AVIARIES));
 
         farm.startFarmDay();
 
     }
+
+    private static List<DogsAviary> generateAviariesWithDogs(int numberOfAviaries) {
+        List<DogsAviary> generatedAviaries = new ArrayList<>(numberOfAviaries);
+        for (int i = 0; i < numberOfAviaries; i++) {
+            DogsAviary dogsAviary = new DogsAviary();
+            dogsAviary.addDog(RandomDogCreator.createRandomGod());
+            generatedAviaries.add(dogsAviary);
+        }
+        return generatedAviaries;
+    }
+
+
 }
